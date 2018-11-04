ca9b0fa66a448ff78865f982adc61d9574d575fd
Łukasz Reszetow <lukasz.reszetow@brightinventions.pl>
2018-11-04 12:53:09 +0100

    Camera permission for color recognition

---
+++ app/src/main/java/com/example/lukaszreszetow/mamproject/ColorRecognitionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lukaszreszetow.mamproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ColorRecognitionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_color_recognition)
+    }
+}
--- app/src/main/java/com/example/lukaszreszetow/mamproject/MainActivity.kt
+++ app/src/main/java/com/example/lukaszreszetow/mamproject/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.lukaszreszetow.mamproject
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.opencv.android.OpenCVLoader
@@ -25,6 +29,31 @@ class MainActivity : AppCompatActivity() {
         sensorsBT.setOnClickListener {
             startActivity(Intent(this, SensorsActivity::class.java))
         }
+
+        colorRecognitionBT.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.CAMERA),
+                        MY_PERMISSION_CAMERA)
+            } else {
+                startActivity(Intent(this, ColorRecognitionActivity::class.java))
+            }
+        }
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            MY_PERMISSION_CAMERA -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    startActivity(Intent(this, ColorRecognitionActivity::class.java))
+            }
+        }
+    }
+
+    companion object {
+        const val MY_PERMISSION_CAMERA = 123
+    }
 }
+
+
