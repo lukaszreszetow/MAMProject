8f9a93da0128d19e68ee0d245c0d4b867d8af64e
Łukasz Reszetow <lukasz.reszetow@brightinventions.pl>
2018-11-04 12:25:04 +0100

    Adding OpenCv to the project

--- app/src/main/java/com/example/lukaszreszetow/mamproject/MainActivity.kt
+++ app/src/main/java/com/example/lukaszreszetow/mamproject/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.lukaszreszetow.mamproject
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupButtons()
+        if (OpenCVLoader.initDebug()) {
+            Toast.makeText(this, "OpenCv zaladowane", Toast.LENGTH_LONG).show()
+
+        } else {
+            Toast.makeText(this, "OpenCv ERROR", Toast.LENGTH_LONG).show()
+        }
     }
 
     private fun setupButtons() {
